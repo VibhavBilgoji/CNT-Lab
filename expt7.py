5262685328309a1faa00a54127fd6e42ffe2459f
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-05-12 10:10:10 +0530

    Add steps to each primality test

--- expt7.py
+++ expt7.py
@@ -10,11 +10,13 @@ def FermatTest(p):
     elif gcd(a, p) != 1:
         print(f"gcd({a}, {p}) != 1, so {p} is composite")
     else:
+        print(f"Check {a}^({p}-1) mod {p}")
         y = pow(a, p-1, p)
-        if y % p != 1:
-            print(f"{y} % {p} != 1, so {p} is composite")
+        print(f"{a}^({p}-1) mod {p} = {y}")
+        if y != 1:
+            print(f"{y} != 1, therefore {p} is composite")
         else:
-            print(f"{p} is probably prime")
+            print(f"{y} = 1, {p} is probably a prime")
 
 def MillerRabinTest(p):
     a = 2
@@ -27,16 +29,21 @@ def MillerRabinTest(p):
         k+=1
         p_minus_1 /= 2
     m = (p-1) // (2 ** k)
+    print(f"Write p-1 as 2^k * m: {p-1} = 2^{k} * {m}")
     b = pow(a, m, p)
+    print(f"Compute initial witness: {a}^{m} mod {p} = {b}")
     if b == 1 or b == p-1:
-        print(f"{p} is probably prime")
+        if(b == 1):
+            print(f"b = 1, therefore {p} is probably a prime")
+        else:
+            print(f"b = p-1, therefore {p} is probably a prime")
         return
     for i in range(k-1):
         b = pow(b, 2, p)
-        if b % p == p-1:
-            print(f"{p} is probably prime")
+        if b == p-1:
+            print(f"b = p-1, therefore {p} is probably a prime")
             return
-    print(f"{p} is composite")
+    print(f"Did not find 1 or n-1. therefore{p} is composite")
 
 def SolovayStrassenTest(p):
     a = 2
@@ -44,12 +51,14 @@ def SolovayStrassenTest(p):
         print(f"gcd({a}, {p}) != 1, so {p} is composite")
         return
     x = 1 if (p**2 - 1) // 8 % 2 == 0 else -1
+    print(f"Calculate Jacobi Symbol ({a}/{p}) = {x}")
     y = pow(a, (p-1) // 2, p)
+    print(f"Compute a^((p-1)/2) mod p: {a}^{p-1} mod {p} = {y}")
     if x == 0:
         print(f"Jacobi symbol ({a}/{p}) = 0, so {p} is composite")
         return
     if y == x % p:
-        print(f"{p} is probably prime")
+        print(f"{y} == {x} mod {p}, therefore {p} is probably a prime")
     else:
         print(f"{y} != {x} mod {p}, so {p} is composite")
 
