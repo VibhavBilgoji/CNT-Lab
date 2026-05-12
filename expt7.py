1e64be78f5a8a97ef117a2e5b7e669fe2e84035b
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-05-12 10:27:18 +0530

    Minor changes to Expt 7 code (Output)

--- expt7.py
+++ expt7.py
@@ -12,11 +12,11 @@ def FermatTest(p):
     else:
         print(f"Check {a}^({p}-1) mod {p}")
         y = pow(a, p-1, p)
-        print(f"{a}^({p}-1) mod {p} = {y}")
+        print(f"{a}^({p}-1) mod {p} = y")
         if y != 1:
-            print(f"{y} != 1, therefore {p} is composite")
+            print(f"y != 1, therefore {p} is composite")
         else:
-            print(f"{y} = 1, {p} is probably a prime")
+            print(f"y = 1, {p} is probably a prime")
 
 def MillerRabinTest(p):
     a = 2
@@ -31,7 +31,7 @@ def MillerRabinTest(p):
     m = (p-1) // (2 ** k)
     print(f"Write p-1 as 2^k * m: {p-1} = 2^{k} * {m}")
     b = pow(a, m, p)
-    print(f"Compute initial witness: {a}^{m} mod {p} = {b}")
+    print(f"Compute initial witness: {a}^{m} mod {p} = b")
     if b == 1 or b == p-1:
         if(b == 1):
             print(f"b = 1, therefore {p} is probably a prime")
@@ -51,16 +51,16 @@ def SolovayStrassenTest(p):
         print(f"gcd({a}, {p}) != 1, so {p} is composite")
         return
     x = 1 if (p**2 - 1) // 8 % 2 == 0 else -1
-    print(f"Calculate Jacobi Symbol ({a}/{p}) = {x}")
+    print(f"Calculate Jacobi Symbol ({a}/{p}) = x")
     y = pow(a, (p-1) // 2, p)
-    print(f"Compute a^((p-1)/2) mod p: {a}^{p-1} mod {p} = {y}")
+    print(f"Compute a^((p-1)/2) mod p: {a}^{p-1} mod {p} = y")
     if x == 0:
         print(f"Jacobi symbol ({a}/{p}) = 0, so {p} is composite")
         return
     if y == x % p:
-        print(f"{y} == {x} mod {p}, therefore {p} is probably a prime")
+        print(f"y == x mod {p}, therefore {p} is probably a prime")
     else:
-        print(f"{y} != {x} mod {p}, so {p} is composite")
+        print(f"y != x mod {p}, so {p} is composite")
 
 if __name__ == "__main__":
     p = int(input("Enter a number (p): "))
