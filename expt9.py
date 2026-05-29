7d1f3e87909d086ab091413d94e467d20dc7652e
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-05-29 22:02:35 +0530

    Add necessary changes in CRT algorithm in Expt 9

--- expt9.py
+++ expt9.py
@@ -1,14 +1,14 @@
 import math
 
-def calcPower(a, b, n, p, q, pow):
+def calcPower(a, b, n, p, q, sym):
     print(f"{a}^({q}x) ≡ {b}^{q} (mod {n})")
-    print(f"{a}^({q}{pow}) × {a}^({p*q}a₁) ≡ {b}^{q} (mod {n})")
-    print(f"{a}^({q}{pow}) ≡ {b}^{q} (mod {n})")
+    print(f"{a}^({q}{sym}) × {a}^({p*q}a₁) ≡ {b}^{q} (mod {n})")
+    print(f"{a}^({q}{sym}) ≡ {b}^{q} (mod {n})")
     aq = pow(a, q, n)
     print(f"{a}^{q} ≡ {aq} (mod {n})")
     bq = pow(b, q, n)
     print(f"{b}^{q} ≡ {bq} (mod {n})")
-    print(f"{aq}^{pow} ≡ {bq} (mod {n})")
+    print(f"{aq}^{sym} ≡ {bq} (mod {n})")
     if bq == 1:
         return 0
     elif aq == bq:
@@ -49,7 +49,7 @@ def pohligHellman(a, b, n):
         return
     print(f"{n} = {p} × {q}")
     print(f"p = {p}, q = {q}\n")
-
+    n += 1 #restore n
     print(f"let x = a₀ + {p}a₁")
     print("Calculating a₀...")
     a0 = calcPower(a, b, n, p, q, "a₀")
@@ -66,7 +66,22 @@ def pohligHellman(a, b, n):
         return
     print(f"x ≡ {b0} (mod {q})\n")
     print("Using CRT to combine results...")
-    #TODO: Implement CRT to combine a0 and b0 to find x mod n
+    print(f"a₁ = {a0}, a₂ = {b0}, M₁ = {q}, M₂ = {p}")
+    y1 = q % p
+    y2 = p % q
+    i = 1
+    while i * y1 % p != 1:
+        i += 1
+    invM1 = i
+    i = 1
+    while i * y2 % q != 1:
+        i += 1
+    invM2 = i
+    print(f"M₁⁻¹ = {invM1} (mod {p}), M₂⁻¹ = {invM2} (mod {q})")
+    x = (a0 * q * invM1 + b0 * p * invM2) % (n-1)
+    print("x = (a₀ × M₁ × M₁⁻¹ + b₀ × M₂ × M₂⁻¹) mod n")
+    print(f"x = ({a0} × {q} × {invM1} + {b0} × {p} × {invM2}) mod {n-1}\n")
+    print(f"The solution to the equation is: x ≡ {x} (mod {n-1})")
 
 while True:
     main()
