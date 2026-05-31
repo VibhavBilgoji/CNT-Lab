7168945a4ac08cf6a64ca710482a0604ab45a5f7
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-05-31 10:22:41 +0530

    Implement Playfair Cipher in program 9 changed toEncrypt function to return boolean input_text no longer global, now passed to every cipher function changed from "vector" to "key" in vigenere cipher switched from strip() to replace(" ", "") accept strings after choosing cipher remove spaces from input_text before sending to cipher functions

--- expt10.py
+++ expt10.py
@@ -1,40 +1,37 @@
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def encrypt_or_decrypt():
+def toEncrypt():
     while True:
         choice = input("Do you want to encrypt or decrypt? (E/D): ").strip().upper()
         if choice in ('E', 'D'):
-            return choice
+            if choice == 'E':
+                return True
+            else:
+                return False
         print("Please enter 'E' for encrypt or 'D' for decrypt.")
 
-def caesar_cipher():
-    mode = encrypt_or_decrypt()
+def caesar_cipher(input_text):
+    mode = toEncrypt()
     k = int(input("Enter the shift value (k): "))
     processed_arr = []
-    if mode == 'E':
+    if mode:
         for c in input_text:
-            if c == ' ':
-                continue
             processed_arr += alpha[(alpha.index(c) + k) % 26]
         processed_text = ''.join(processed_arr)
         print("Ciphertext:", processed_text)
     else:
         for c in input_text:
-            if c == ' ':
-                continue
             processed_arr += alpha[(alpha.index(c) - k) % 26]
         processed_text = ''.join(processed_arr)
         print("Plaintext:", processed_text)
 
-def affine_cipher():
-    mode = encrypt_or_decrypt()
+def affine_cipher(input_text):
+    mode = toEncrypt()
     a = int(input("Enter value of α: "))
     b = int(input("Enter value of β: "))
     processed_arr = []
-    if mode == 'E':
+    if mode:
         for c in input_text:
-            if c == ' ':
-                continue
             processed_arr += alpha[(a * alpha.index(c) + b) % 26]
         processed_text = ''.join(processed_arr)
         print("Ciphertext:", processed_text)
@@ -45,43 +42,114 @@ def affine_cipher():
         a = inv
         b = (-b) * inv % 26
         for c in input_text:
-            if c == ' ':
-                continue
             processed_arr += alpha[(a * alpha.index(c) + b) % 26]
         processed_text = ''.join(processed_arr)
         print("Plaintext:", processed_text)
 
-def vigenere_cipher():
-    mode = encrypt_or_decrypt()
-    vector = input("Enter the vector: ").strip().upper()
-    n = len(vector)
+def vigenere_cipher(input_text):
+    mode = toEncrypt()
+    key = input("Enter the key: ").replace(" ", "").upper()
+    n = len(key)
     i = 0
     processed_arr = []
-    if mode == 'E':
+    if mode:
         for c in input_text:
-            if c == ' ':
-                continue
-            offset = alpha.index(vector[i % n])
+            offset = alpha.index(key[i % n])
             processed_arr += alpha[(alpha.index(c) + offset) % 26]
             i += 1
         processed_text = ''.join(processed_arr)
         print("Ciphertext: " + processed_text)
     else:
         for c in input_text:
-            if c == ' ':
-                continue
-            offset = alpha.index(vector[i % n])
+            offset = alpha.index(key[i % n])
             processed_arr += alpha[(alpha.index(c) - offset) % 26]
             i += 1
         processed_text = ''.join(processed_arr)
         print("Ciphertext: " + processed_text)
 
-def playfair_cipher():
-    # mode = encrypt_or_decrypt()
-    pass
+def playfair_cipher(input_text):
+    mode = toEncrypt()
+    key = input("Enter the key: ").replace(" ", "").upper().replace("J", "I")
+    input_text = input_text.replace("J", "I")
+    seen_chars = set('J')
+    matrix = list()
+    for c in key:
+        if c not in seen_chars:
+            seen_chars.add(c)
+            matrix += c
+    for c in alpha:
+        if c not in seen_chars:
+            seen_chars.add(c)
+            matrix += c
+    n = len(input_text)
+    print("\nMatrix:")
+    for i in range(5):
+        for j in range(5):
+            if(matrix[i*5 + j] == 'I'):
+                print("Ĳ", end = " ")
+            else:
+                print(matrix[i*5 + j], end = " ")
+        print()
+    i = 0
+    processed_arr = []
+    if mode:
+        while i < n:
+            a = input_text[i]
+            b = input_text[i+1] if i < (n-1) else 'X'
+            if a == b:
+                b = 'X'
+                i += 1
+            else:
+                i += 2
+            xj = xk = yj = yk = 0
+            for j in range(5):
+                for k in range(5):
+                    if matrix[j*5 + k] == a:
+                        xj = j
+                        xk = k
+                    elif matrix[j*5 + k] == b:
+                        yj = j
+                        yk = k
+            if xj == yj: #same row
+                ao = matrix[xj*5 + ((xk+1) % 5)]
+                bo = matrix[yj*5 + ((yk+1) % 5)]
+            elif xk == yk: #same column
+                ao = matrix[((xj+1) % 5)*5 + xk]
+                bo = matrix[((yj+1) % 5)*5 + yk]
+            else:
+                ao = matrix[xj*5 + yk]
+                bo = matrix[yj*5 + xk]
+            processed_arr += f"{ao}{bo}"
+        processed_text = ''.join(processed_arr)
+        print("\nCiphertext: " + processed_text)
+    else:
+        while i < n:
+            a = input_text[i]
+            b = input_text[i+1]
+            xj = xk = yj = yk = 0
+            for j in range(5):
+                for k in range(5):
+                    if matrix[j*5 + k] == a:
+                        xj = j
+                        xk = k
+                    elif matrix[j*5 + k] == b:
+                        yj = j
+                        yk = k
+            if xj == yj: #same row
+                ao = matrix[xj*5 + ((xk-1) % 5)]
+                bo = matrix[yj*5 + ((yk-1) % 5)]
+            elif xk == xk: #same column
+                ao = matrix[((xj-1) % 5)*5 + xk]
+                bo = matrix[((yj-1) % 5)*5 + yk]
+            else:
+                ao = matrix[xj*5 + yk]
+                bo = matrix[yj*5 + xk]
+            processed_arr += f"{ao}{bo}"
+        processed_text = ''.join(processed_arr)
+        print("\nPlaintext: " + processed_text)
 
-def adfgx_cipher():
-    # mode = encrypt_or_decrypt()
+def adfgx_cipher(input_text):
+    # mode = toEncrypt()
     pass
 
 def choose():
@@ -96,16 +164,17 @@ def choose():
             print("Please enter an integer between 1 and 5.")
 
 def call_cipher(choice):
+    input_text = input("\nEnter text: ").replace(" ", "").upper()
     if choice == 1:
-        caesar_cipher()
+        caesar_cipher(input_text)
     elif choice == 2:
-        affine_cipher()
+        affine_cipher(input_text)
     elif choice == 3:
-        vigenere_cipher()
+        vigenere_cipher(input_text)
     elif choice == 4:
-        playfair_cipher()
+        playfair_cipher(input_text)
     elif choice == 5:
-        adfgx_cipher()
+        adfgx_cipher(input_text)
 
 def choose_at_end():
     print("\nChoose option:\n1. Enter another text\n2. Choose another cipher")
@@ -120,13 +189,11 @@ def choose_at_end():
 
 def run_program():
     while True:
-        global input_text
-        input_text = input("\nEnter text: ").upper()
+        choice = choose()
         while True:
-            choice = choose()
             call_cipher(choice)
             next_step = choose_at_end()
-            if next_step == 1:
+            if next_step == 2:
                 break
 
 if __name__ == "__main__":
