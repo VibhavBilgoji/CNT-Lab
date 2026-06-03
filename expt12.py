9bcc16fd04b5eccd4153b2e9b0497a52281b6136
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-06-03 08:35:13 +0530

    Code Cleanup in Expt 12

--- expt12.py
+++ expt12.py
@@ -2,25 +2,22 @@ def key_exchange():
     q = int(input("Enter a prime number (q): "))
     g = int(input("Enter a alpha (α): "))
 
-    a = int(input("Enter Alice's private key: "))
-    b = int(input("Enter Bob's private key: "))
+    xa = int(input("Enter Alice's private key: "))
+    xb = int(input("Enter Bob's private key: "))
 
-    A = pow(g, a, q)
-    B = pow(g, b, q)
+    ya = pow(g, xa, q)
+    yb = pow(g, xb, q)
 
-    print("\nAlice's public key:", A)
-    print("Bob's public key:", B)
+    print("\nAlice's public key:", ya)
+    print("Bob's public key:", yb)
 
-    alice_secret = pow(B, a, q)
-    bob_secret = pow(A, b, q)
+    ka = pow(yb, xa, q)
+    kb = pow(ya, xb, q)
 
-    print("\nAlice's shared secret:", alice_secret)
-    print("Bob's shared secret:", bob_secret)
+    print("\nAlice's shared secret:", ka)
+    print("Bob's shared secret:", kb)
 
-    if alice_secret == bob_secret:
-        print("Key exchange successful!")
-    else:
-        print("Key exchange failed!")
+    print("Key exchange " + "Successful!" if ka == kb else "Failed!")
 
 if __name__ == "__main__":
     try:
